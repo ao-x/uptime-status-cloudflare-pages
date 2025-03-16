461e017f05b2f38d962f04b14132bfbcb55492a6
梦文博 <admin@ao-x.cn>
2025-03-16 23:54:35 +0800

    适配

--- public/config.js
+++ public/config.js
@@ -1,13 +1,17 @@
-// 配置
+// 使用 process.env 访问环境变量
+const getEnvVar = (key, defaultValue) => {
+  // Cloudflare Workers 使用 `process.env` 来访问环境变量
+  return process.env[key] !== undefined ? process.env[key] : defaultValue;
+};
+
 window.Config = {
-  SiteName: process.env.SITE_NAME || '站点监测页',
-  ApiKeys: process.env.UPTIMEROBOT_API_KEYS?.split(',') || [],
-  CountDays: parseInt(process.env.COUNT_DAYS) || 30,
-  ShowLink: process.env.SHOW_LINK === 'true',
-  Navi: [
-    {
-      text: 'GitHub',
-      url: 'https://github.com/ao-x/uptime-status-cloudflare-pages'
-    },
-  ],
+  // 使用环境变量或提供默认值
+  SiteName: getEnvVar('SITE_NAME', 'AO-X 站点监测'),
+  ApiKeys: JSON.parse(getEnvVar('API_KEYS', '["ur2012830-87a31646e0e36ccca208da18"]')),
+  CountDays: parseInt(getEnvVar('COUNT_DAYS', 60), 10),
+  ShowLink: getEnvVar('SHOW_LINK', 'true') === 'true',
+  Navi: [{
+    text: 'GitHub',
+    url: 'https://github.com/ao-x/uptime-status-cloudflare-pages'
+  }]
 };
